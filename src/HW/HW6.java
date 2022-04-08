eb283cfd2caa2320a3157427e999946defbc7514
Victor <victor.goltsov@gmail.com>
2022-04-08 22:23:22 +0800

    1 HW commit

--- src/HW/HW6.java
+++ src/HW/HW6.java
@@ -22,20 +22,23 @@ public class HW6 {
         //6.2.2 Необходимо создать класс Employee с полями как у Person (из предыдущего задания) и поле зарплата.
         // Класс должен иметь метод isSameName(Employee employee) который возвращает true, если у сотрудника у которого
         // был вызван метод и сотрудника который был передан как параметр, одинаковое имя.
-        HW6_Employee marpl2 = new HW6_Employee("Murpol", 80, "f", 500);
+        HW6_Employee marpl2 = new HW6_Employee("Murpol", 80, "f", 50);
         System.out.println(marpl2.isNameEqualToParam("murpol"));
-        HW6_Employee uotson2 = new HW6_Employee("Uotson", 68, "m", 700);
+        HW6_Employee uotson2 = new HW6_Employee("Uotson", 68, "m", 70);
         System.out.println(marpl2.isNameEqualToParam("UotsoNn"));
+        HW6_Employee john = new HW6_Employee("John", 50, "m", 60);
+        HW6_Employee tanya = new HW6_Employee("Tanya", 40, "f", 70);
+        HW6_Employee janna = new HW6_Employee("Janna", 63, "f", 90);
+        HW6_Employee mikal = new HW6_Employee("Mikal", 40, "m", 80);
+
 
         //6.2.3 Необходимо создать класс Salary с единственным методом - getSum(Employee[] employeeArray), метод должен
         // возвращать сумму зарплат всех сотрудников из массива переданного в качестве аргумента вызова метода.
-        HW6_Salary john = new HW6_Salary("John", 50, "m", 75000);
-        HW6_Salary tanya = new HW6_Salary("Tanya", 40, "f", 70000);
-        HW6_Salary janna = new HW6_Salary("Janna", 63, "f", 73000);
-        HW6_Salary mikal = new HW6_Salary("Mikal", 40, "m", 85000);
 
-        HW6_Salary[] arr = {john, tanya, janna};
-        System.out.println(HW6_Salary.sumSalary(arr));
+
+        HW6_Employee[] arr = {marpl2, tanya, janna};
+        HW6_Salary salary = new HW6_Salary();
+        System.out.println(salary.getSum(arr));
         //System.out.println(HW6_Salary.sumSalary(new HW6_Salary[]{john, tanya, janna}));  //или вот такая запись
     }
 }
--- src/Lessons/HW6_Salary.java
+++ src/Lessons/HW6_Salary.java
@@ -1,22 +1,23 @@
 package Lessons;
 
 public class HW6_Salary {
-    private String name;
-    private int age;
-    private String gender;
-    private int salary;
+//    private String name;
+  //  private int age;
+    //private String gender;
+    //private int salary;
 
-   public HW6_Salary (String name, int age, String gender, int salary) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.salary = salary;
-    }
+ //  public HW6_Salary (String name, int age, String gender, int salary) {
+  //   this.name = name;
+   //  this.age = age;
+    // this.gender = gender;
+     //this.salary = salary;
+   //}
 
-    public static int sumSalary (HW6_Salary[]emp) {
+    public int getSum (HW6_Employee[] arr) {
+
         int sum = 0;
-        for (int i = 0; i < emp.length; i++) {
-            sum = sum + emp[i].salary;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i].getSalary();
         }
         return sum;
     }
